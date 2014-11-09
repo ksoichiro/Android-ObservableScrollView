fa9009fe1b754c1f1963a6ba8b4414b0b968d887
ksoichiro <soichiro.kashima@gmail.com>
2014-11-09 12:04:37 +0900

    Add quick return toolbar, sticky view and list view sample.

---
+++ observablescrollview-samples/src/main/java/com/github/ksoichiro/android/observablescrollview/samples/ToolbarControlListViewActivity.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2014 Soichiro Kashima
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.ksoichiro.android.observablescrollview.samples;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import com.github.ksoichiro.android.observablescrollview.ObservableListView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
+import com.nineoldandroids.view.ViewHelper;
+import com.nineoldandroids.view.ViewPropertyAnimator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToolbarControlListViewActivity extends ActionBarActivity implements ObservableScrollViewCallbacks {
+
+    private View mHeaderView;
+    private View mToolbarView;
+    private ObservableListView mListView;
+    private boolean mScrollBegan;
+    private boolean mDragging;
+    private int mBaseTranslationY;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbarcontrollistview);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        mHeaderView = findViewById(R.id.header);
+        mToolbarView = findViewById(R.id.toolbar);
+
+        mListView = (ObservableListView) findViewById(R.id.list);
+        mListView.setScrollViewCallbacks(this);
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        mListView.addHeaderView(inflater.inflate(R.layout.padding, null)); // toolbar
+        mListView.addHeaderView(inflater.inflate(R.layout.padding, null)); // sticky view
+        List<String> items = new ArrayList<String>();
+        for (int i = 1; i <= 100; i++) {
+            items.add("Item " + i);
+        }
+        mListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
+    }
+
+    @Override
+    public void onScrollChanged(int scrollY) {
+        if (mDragging) {
+            int toolbarHeight = mToolbarView.getHeight();
+            if (mScrollBegan) {
+                mScrollBegan = false;
+                float currentHeaderTranslationY = ViewHelper.getTranslationY(mHeaderView);
+                if (-toolbarHeight < currentHeaderTranslationY && toolbarHeight < scrollY) {
+                    mBaseTranslationY = scrollY;
+                }
+            }
+            int headerTranslationY = Math.min(0, Math.max(-toolbarHeight, -(scrollY - mBaseTranslationY)));
+            ViewPropertyAnimator.animate(mHeaderView).cancel();
+            ViewHelper.setTranslationY(mHeaderView, headerTranslationY);
+        }
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+        mScrollBegan = mDragging = true;
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+        mDragging = false;
+        mBaseTranslationY = 0;
+
+        float headerTranslationY = ViewHelper.getTranslationY(mHeaderView);
+        int toolbarHeight = mToolbarView.getHeight();
+        if (scrollState == ScrollState.UP) {
+            if (toolbarHeight < mListView.getCurrentScrollY()) {
+                if (headerTranslationY != -toolbarHeight) {
+                    ViewPropertyAnimator.animate(mHeaderView).cancel();
+                    ViewPropertyAnimator.animate(mHeaderView).translationY(-toolbarHeight).setDuration(200).start();
+                }
+            }
+        } else if (scrollState == ScrollState.DOWN) {
+            if (toolbarHeight < mListView.getCurrentScrollY()) {
+                if (headerTranslationY != 0) {
+                    ViewPropertyAnimator.animate(mHeaderView).cancel();
+                    ViewPropertyAnimator.animate(mHeaderView).translationY(0).setDuration(200).start();
+                }
+            }
+        }
+    }
+}
