ad2356dca5d770d89d81061347d282f01266e5dd
ksoichiro <soichiro.kashima@gmail.com>
2014-11-11 06:59:17 +0900

    Add GridView support.
    GridView doesn't have headers, so there is no toolbar sample for now.

---
+++ observablescrollview-samples/src/main/java/com/github/ksoichiro/android/observablescrollview/samples/ActionBarControlGridViewActivity.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2014 Soichiro Kashima
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.ksoichiro.android.observablescrollview.samples;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ArrayAdapter;
+
+import com.github.ksoichiro.android.observablescrollview.ObservableGridView;
+import com.github.ksoichiro.android.observablescrollview.ObservableListView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActionBarControlGridViewActivity extends ActionBarActivity implements ObservableScrollViewCallbacks {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actionbarcontrolgridview);
+
+        ObservableGridView gridView = (ObservableGridView) findViewById(R.id.grid);
+        gridView.setScrollViewCallbacks(this);
+        List<String> items = new ArrayList<String>();
+        for (int i = 1; i <= 100; i++) {
+            items.add("Item " + i);
+        }
+        gridView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
+    }
+
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+        ActionBar ab = getSupportActionBar();
+        if (scrollState == ScrollState.UP) {
+            if (ab.isShowing()) {
+                ab.hide();
+            }
+        } else if (scrollState == ScrollState.DOWN) {
+            if (!ab.isShowing()) {
+                ab.show();
+            }
+        }
+    }
+}
---
+++ observablescrollview/src/main/java/com/github/ksoichiro/android/observablescrollview/ObservableGridView.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2014 Soichiro Kashima
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.ksoichiro.android.observablescrollview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.SparseIntArray;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.GridView;
+
+import com.github.ksoichiro.android.observablescrollview.internal.LogUtils;
+
+public class ObservableGridView extends GridView {
+    private static final String TAG = ObservableGridView.class.getSimpleName();
+
+    private ObservableScrollViewCallbacks mCallbacks;
+    private int mPrevFirstVisiblePosition;
+    private int mPrevFirstVisibleChildHeight = -1;
+    private int mPrevScrolledChildrenHeight;
+    private SparseIntArray mChildrenHeights;
+    private int mPrevScrollY;
+    private int mScrollY;
+    private ScrollState mScrollState;
+    private boolean mFirstScroll;
+    private boolean mDragging;
+
+    public ObservableGridView(Context context) {
+        super(context);
+        init();
+    }
+
+    public ObservableGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ObservableGridView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
+        if (mCallbacks != null) {
+            if (getChildCount() > 0) {
+                int firstVisiblePosition = getFirstVisiblePosition();
+                for (int i = getFirstVisiblePosition(), j = 0; i <= getLastVisiblePosition(); i++, j++) {
+                    if (mChildrenHeights.indexOfKey(i) < 0 || getChildAt(j).getHeight() != mChildrenHeights.get(i)) {
+                        mChildrenHeights.put(i, getChildAt(j).getHeight());
+                    }
+                }
+
+                View firstVisibleChild = getChildAt(0);
+                if (firstVisibleChild != null) {
+                    if (mPrevFirstVisiblePosition < firstVisiblePosition) {
+                        // scroll down
+                        int skippedChildrenHeight = 0;
+                        if (firstVisiblePosition - mPrevFirstVisiblePosition != 1) {
+                            LogUtils.v(TAG, "Skipped some children while scrolling down: " + (firstVisiblePosition - mPrevFirstVisiblePosition));
+                            for (int i = firstVisiblePosition - 1; i > mPrevFirstVisiblePosition; i--) {
+                                if (0 < mChildrenHeights.indexOfKey(i)) {
+                                    skippedChildrenHeight += mChildrenHeights.get(i);
+                                    LogUtils.v(TAG, "Calculate skipped child height at " + i + ": " + mChildrenHeights.get(i));
+                                } else {
+                                    LogUtils.v(TAG, "Could not calculate skipped child height at " + i);
+                                }
+                            }
+                        }
+                        mPrevScrolledChildrenHeight += mPrevFirstVisibleChildHeight + skippedChildrenHeight;
+                        mPrevFirstVisibleChildHeight = firstVisibleChild.getHeight();
+                    } else if (firstVisiblePosition < mPrevFirstVisiblePosition) {
+                        // scroll up
+                        int skippedChildrenHeight = 0;
+                        if (mPrevFirstVisiblePosition - firstVisiblePosition != 1) {
+                            LogUtils.v(TAG, "Skipped some children while scrolling up: " + (mPrevFirstVisiblePosition - firstVisiblePosition));
+                            for (int i = mPrevFirstVisiblePosition - 1; i > firstVisiblePosition; i--) {
+                                if (0 < mChildrenHeights.indexOfKey(i)) {
+                                    skippedChildrenHeight += mChildrenHeights.get(i);
+                                    LogUtils.v(TAG, "Calculate skipped child height at " + i + ": " + mChildrenHeights.get(i));
+                                } else {
+                                    LogUtils.v(TAG, "Could not calculate skipped child height at " + i);
+                                }
+                            }
+                        }
+                        mPrevScrolledChildrenHeight -= firstVisibleChild.getHeight() + skippedChildrenHeight;
+                        mPrevFirstVisibleChildHeight = firstVisibleChild.getHeight();
+                    } else if (firstVisiblePosition == 0) {
+                        mPrevFirstVisibleChildHeight = firstVisibleChild.getHeight();
+                    }
+                    if (mPrevFirstVisibleChildHeight < 0) {
+                        mPrevFirstVisibleChildHeight = 0;
+                    }
+                    mScrollY = mPrevScrolledChildrenHeight - firstVisibleChild.getTop();
+                    mPrevFirstVisiblePosition = firstVisiblePosition;
+
+                    LogUtils.v(TAG, "first: " + firstVisiblePosition + " scrollY: " + mScrollY + " first height: " + firstVisibleChild.getHeight() + " first top: " + firstVisibleChild.getTop());
+                    mCallbacks.onScrollChanged(mScrollY, mFirstScroll, mDragging);
+                    if (mFirstScroll) {
+                        mFirstScroll = false;
+                    }
+
+                    if (mPrevScrollY < mScrollY) {
+                        //down
+                        mScrollState = ScrollState.UP;
+                    } else if (mScrollY < mPrevScrollY) {
+                        //up
+                        mScrollState = ScrollState.DOWN;
+                    } else {
+                        mScrollState = ScrollState.STOP;
+                    }
+                    mPrevScrollY = mScrollY;
+                } else {
+                    LogUtils.v(TAG, "first: " + firstVisiblePosition);
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (mCallbacks != null) {
+            switch (ev.getActionMasked()) {
+                case MotionEvent.ACTION_DOWN:
+                    mFirstScroll = mDragging = true;
+                    mCallbacks.onDownMotionEvent();
+                    break;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    mDragging = false;
+                    mCallbacks.onUpOrCancelMotionEvent(mScrollState);
+                    break;
+            }
+        }
+        return super.onTouchEvent(ev);
+    }
+
+    public void setScrollViewCallbacks(ObservableScrollViewCallbacks listener) {
+        mCallbacks = listener;
+    }
+
+    public int getCurrentScrollY() {
+        return mScrollY;
+    }
+
+    private void init() {
+        mChildrenHeights = new SparseIntArray();
+    }
+}
