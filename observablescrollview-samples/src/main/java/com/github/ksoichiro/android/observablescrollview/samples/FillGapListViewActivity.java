a7a88e109d8352712a1d1fa4fe15870fe2c38e67
Soichiro Kashima <soichiro.kashima@gmail.com>
2014-12-16 22:00:39 +0900

    Add example of filling gap effect using ListView. Related to issue #15.

---
+++ observablescrollview-samples/src/main/java/com/github/ksoichiro/android/observablescrollview/samples/FillGapListViewActivity.java
@@ -0,0 +1,200 @@
+/*
+ * Copyright 2014 Soichiro Kashima
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.ksoichiro.android.observablescrollview.samples;
+
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.github.ksoichiro.android.observablescrollview.ObservableListView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
+import com.nineoldandroids.animation.ValueAnimator;
+import com.nineoldandroids.view.ViewHelper;
+import com.nineoldandroids.view.ViewPropertyAnimator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FillGapListViewActivity extends ActionBarActivity implements ObservableScrollViewCallbacks {
+
+    private View mImageHolder;
+    private View mHeader;
+    private View mHeaderBar;
+    private View mHeaderBackground;
+    private View mListBackgroundView;
+    private ObservableListView mListView;
+    private int mActionBarSize;
+    private int mFlexibleSpaceImageHeight;
+    private int mIntersectionHeight;
+    private int mPrevScrollY;
+    private boolean mGapFilled;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fillgaplistview);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        mFlexibleSpaceImageHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_image_height);
+        mIntersectionHeight = getResources().getDimensionPixelSize(R.dimen.intersection_height);
+        mActionBarSize = getActionBarSize();
+
+        mImageHolder = findViewById(R.id.image_holder);
+        mHeader = findViewById(R.id.header);
+        mHeaderBar = findViewById(R.id.header_bar);
+        mHeaderBackground = findViewById(R.id.header_background);
+
+        mListView = (ObservableListView) findViewById(R.id.scroll);
+        mListView.setScrollViewCallbacks(this);
+        List<String> items = new ArrayList<String>();
+        for (int i = 1; i <= 100; i++) {
+            items.add("Item " + i);
+        }
+        mListView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items));
+
+        View paddingView = new View(this);
+        paddingView.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT,
+                mFlexibleSpaceImageHeight));
+        paddingView.setMinimumHeight(mFlexibleSpaceImageHeight);
+        // This is required to disable header's list selector effect
+        paddingView.setClickable(true);
+        mListView.addHeaderView(paddingView);
+
+        // mListBackgroundView makes ListView's background except header view.
+        mListBackgroundView = findViewById(R.id.list_background);
+        final View contentView = getWindow().getDecorView().findViewById(android.R.id.content);
+        contentView.post(new Runnable() {
+            @Override
+            public void run() {
+                // mListBackgroundView's should fill its parent vertically
+                // but the height of the content view is 0 on 'onCreate'.
+                // So we should get it with post().
+                mListBackgroundView.getLayoutParams().height = contentView.getHeight();
+            }
+        });
+
+        ((TextView) findViewById(R.id.title)).setText(getTitle());
+        setTitle(null);
+
+        ViewTreeObserver vto = mListView.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                    mListView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                } else {
+                    mListView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+                onScrollChanged(0, false, false);
+            }
+        });
+    }
+
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+        // Translate image
+        ViewHelper.setTranslationY(mImageHolder, -scrollY / 2);
+
+        // Translate header
+        final int headerHeight = mHeaderBar.getHeight();
+        int headerTranslationY = mActionBarSize - mIntersectionHeight;
+        if (0 <= -scrollY + mFlexibleSpaceImageHeight - headerHeight - mActionBarSize + mIntersectionHeight) {
+            headerTranslationY = -scrollY + mFlexibleSpaceImageHeight - headerHeight;
+        }
+        ViewHelper.setTranslationY(mHeader, headerTranslationY);
+
+        // Translate list background
+        ViewHelper.setTranslationY(mListBackgroundView, headerTranslationY);
+
+        // Show/hide gap
+        boolean scrollUp = mPrevScrollY < scrollY;
+        if (scrollUp) {
+            if (mFlexibleSpaceImageHeight - headerHeight - mActionBarSize <= scrollY) {
+                if (!mGapFilled) {
+                    mGapFilled = true;
+                    hideGap();
+                }
+            }
+        } else {
+            if (scrollY <= mFlexibleSpaceImageHeight - headerHeight - mActionBarSize) {
+                if (mGapFilled) {
+                    mGapFilled = false;
+                    showGap();
+                }
+            }
+        }
+        mPrevScrollY = scrollY;
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+    }
+
+    private void showGap() {
+        changeHeaderBackgroundHeight(mHeaderBar.getHeight() + mActionBarSize, mHeaderBar.getHeight());
+    }
+
+    private void hideGap() {
+        changeHeaderBackgroundHeight(mHeaderBar.getHeight(), mHeaderBar.getHeight() + mActionBarSize);
+    }
+
+    private void changeHeaderBackgroundHeight(float from, float to) {
+        ViewPropertyAnimator.animate(mHeaderBackground).cancel();
+        ValueAnimator a = ValueAnimator.ofFloat(from, to);
+        a.setDuration(100);
+        a.setInterpolator(new AccelerateDecelerateInterpolator());
+        a.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float height = (float) animation.getAnimatedValue();
+                FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) mHeaderBackground.getLayoutParams();
+                lp.height = (int) height;
+                lp.topMargin = (int) (mHeaderBar.getHeight() - height);
+                mHeaderBackground.requestLayout();
+            }
+        });
+        a.start();
+    }
+
+    private int getActionBarSize() {
+        TypedValue typedValue = new TypedValue();
+        int[] textSizeAttr = new int[]{R.attr.actionBarSize};
+        int indexOfAttrTextSize = 0;
+        TypedArray a = obtainStyledAttributes(typedValue.data, textSizeAttr);
+        int actionBarSize = a.getDimensionPixelSize(indexOfAttrTextSize, -1);
+        a.recycle();
+        return actionBarSize;
+    }
+}
