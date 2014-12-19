669b35d95d03abd904abfaf4d19fff386ddce0e7
Soichiro Kashima <soichiro.kashima@gmail.com>
2014-12-20 01:16:42 +0900

    Add example of moving layout and scrolling from a threshold y using RecyclerView.

---
+++ observablescrollview-samples/src/main/java/com/github/ksoichiro/android/observablescrollview/samples/SlidingUpRecyclerViewActivity.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2014 Soichiro Kashima
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.ksoichiro.android.observablescrollview.samples;
+
+import android.annotation.TargetApi;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.AbsListView;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.github.ksoichiro.android.observablescrollview.ObservableRecyclerView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
+import com.nineoldandroids.view.ViewHelper;
+
+import java.util.ArrayList;
+
+public class SlidingUpRecyclerViewActivity extends ActionBarActivity implements ObservableScrollViewCallbacks {
+
+    private View mHeader;
+    private TextView mTitle;
+    private ObservableRecyclerView mRecyclerView;
+    private TouchInterceptionFrameLayout mInterceptionLayout;
+    private int mActionBarSize;
+    private int mIntersectionHeight;
+    private int mHeaderBarHeight;
+    private float mScrollYOnDownMotion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slidinguprecyclerview);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        mIntersectionHeight = getResources().getDimensionPixelSize(R.dimen.intersection_height);
+        mHeaderBarHeight = getResources().getDimensionPixelSize(R.dimen.header_bar_height);
+        mActionBarSize = getActionBarSize();
+
+        mHeader = findViewById(R.id.header);
+
+        mRecyclerView = (ObservableRecyclerView) findViewById(R.id.scroll);
+        mRecyclerView.setScrollViewCallbacks(this);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setHasFixedSize(false);
+        ArrayList<String> items = new ArrayList<String>();
+        for (int i = 1; i <= 100; i++) {
+            items.add("Item " + i);
+        }
+
+        View paddingView = new View(this);
+        paddingView.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT,
+                mHeaderBarHeight));
+        paddingView.setMinimumHeight(mHeaderBarHeight);
+        // This is required to disable header's list selector effect
+        paddingView.setClickable(true);
+        mRecyclerView.setAdapter(new SimpleHeaderRecyclerAdapter(this, items, paddingView));
+
+        mInterceptionLayout = (TouchInterceptionFrameLayout) findViewById(R.id.scroll_wrapper);
+        mInterceptionLayout.setScrollInterceptionListener(mInterceptionListener);
+        mTitle = (TextView) findViewById(R.id.title);
+        mTitle.setText(getTitle());
+        ViewHelper.setTranslationY(mTitle, (mHeaderBarHeight - mActionBarSize) / 2);
+        setTitle(null);
+
+        ViewTreeObserver vto = mInterceptionLayout.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                    mInterceptionLayout.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                } else {
+                    mInterceptionLayout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+                ViewHelper.setTranslationY(mInterceptionLayout, getScreenHeight() - mHeaderBarHeight);
+            }
+        });
+    }
+
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+        // Translate header (fixed)
+        ViewHelper.setTranslationY(mHeader, 0);
+
+        // Translate title (fixed)
+        ViewHelper.setTranslationY(mTitle, mIntersectionHeight);
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+    }
+
+    private TouchInterceptionFrameLayout.TouchInterceptionListener mInterceptionListener = new TouchInterceptionFrameLayout.TouchInterceptionListener() {
+        @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+        @Override
+        public boolean shouldInterceptTouchEvent(MotionEvent ev, boolean moving, float diffY) {
+            final int minInterceptionLayoutY = -mIntersectionHeight;
+            return minInterceptionLayoutY < (int) ViewHelper.getY(mInterceptionLayout)
+                    || !moving
+                    // canScrollVertically is API level 14
+                    || !mRecyclerView.canScrollVertically((int) -diffY);
+        }
+
+        @Override
+        public void onDownMotionEvent(MotionEvent ev) {
+            mScrollYOnDownMotion = mRecyclerView.getCurrentScrollY();
+        }
+
+        @Override
+        public void onMoveMotionEvent(MotionEvent ev, float diffY) {
+            float translationY = ViewHelper.getTranslationY(mInterceptionLayout) - mScrollYOnDownMotion + diffY;
+            if (translationY < -mIntersectionHeight) {
+                translationY = -mIntersectionHeight;
+            } else if (getScreenHeight() - mHeaderBarHeight < translationY) {
+                translationY = getScreenHeight() - mHeaderBarHeight;
+            }
+
+            ViewHelper.setTranslationY(mInterceptionLayout, translationY);
+
+            if (translationY < 0) {
+                FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) mInterceptionLayout.getLayoutParams();
+                lp.height = (int) -translationY + getScreenHeight();
+                mInterceptionLayout.requestLayout();
+            }
+
+            // Translate title
+            float hiddenHeight = translationY < 0 ? -translationY : 0;
+            ViewHelper.setTranslationY(mTitle, Math.min(mIntersectionHeight, (mHeaderBarHeight + hiddenHeight - mActionBarSize) / 2));
+        }
+
+        @Override
+        public void onUpOrCancelMotionEvent(MotionEvent ev) {
+        }
+    };
+
+    private int getActionBarSize() {
+        TypedValue typedValue = new TypedValue();
+        int[] textSizeAttr = new int[]{R.attr.actionBarSize};
+        int indexOfAttrTextSize = 0;
+        TypedArray a = obtainStyledAttributes(typedValue.data, textSizeAttr);
+        int actionBarSize = a.getDimensionPixelSize(indexOfAttrTextSize, -1);
+        a.recycle();
+        return actionBarSize;
+    }
+
+    private int getScreenHeight() {
+        return findViewById(android.R.id.content).getHeight();
+    }
+}
