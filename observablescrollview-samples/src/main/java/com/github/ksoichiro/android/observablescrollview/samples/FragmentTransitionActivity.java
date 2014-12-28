6d3c0e1d7d82d066185f07a2f007a313821673ac
Soichiro Kashima <soichiro.kashima@gmail.com>
2014-12-28 15:54:23 +0900

    Add an example of fragment transition related to the issue #29.
    This doesn't include Observable*View.

---
+++ observablescrollview-samples/src/main/java/com/github/ksoichiro/android/observablescrollview/samples/FragmentTransitionActivity.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2014 Soichiro Kashima
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.ksoichiro.android.observablescrollview.samples;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * This doesn't include ObservableScrollViews,
+ * just shows how to show/hide Toolbar on the parent Activity of Fragment
+ * to help you implement a screen that uses Fragments.
+ */
+public class FragmentTransitionActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragmenttransition);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        initFragment();
+    }
+
+    /**
+     * Fragment should be added programmatically.
+     * Using fragment tag in XML causes IllegalStateException on rotation of screen
+     * or restoring states of activity.
+     */
+    private void initFragment() {
+        FragmentManager fm = getSupportFragmentManager();
+        if (fm.findFragmentByTag(FragmentTransitionDefaultFragment.FRAGMENT_TAG) == null) {
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.add(R.id.fragment, new FragmentTransitionDefaultFragment(), FragmentTransitionDefaultFragment.FRAGMENT_TAG);
+            ft.commit();
+            fm.executePendingTransactions();
+        }
+    }
+}
---
+++ observablescrollview-samples/src/main/java/com/github/ksoichiro/android/observablescrollview/samples/FragmentTransitionDefaultFragment.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2014 Soichiro Kashima
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.ksoichiro.android.observablescrollview.samples;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This is a fragment to cause fragment transition(replacement).
+ */
+public class FragmentTransitionDefaultFragment extends Fragment {
+
+    public static final String FRAGMENT_TAG = "default";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fragmenttransition_default, container, false);
+
+        ListView listView = (ListView) view.findViewById(R.id.list);
+        List<String> items = new ArrayList<String>();
+        for (int i = 1; i <= 100; i++) {
+            items.add("Item " + i);
+        }
+        listView.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                showNextFragment();
+            }
+        });
+        return view;
+    }
+
+    private void showNextFragment() {
+        FragmentActivity activity = getActivity();
+        if (activity == null) {
+            return;
+        }
+        FragmentManager fm = activity.getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.replace(R.id.fragment, new FragmentTransitionSecondFragment(), FragmentTransitionSecondFragment.FRAGMENT_TAG);
+        ft.addToBackStack(null);
+        ft.commit();
+        fm.executePendingTransactions();
+    }
+}
