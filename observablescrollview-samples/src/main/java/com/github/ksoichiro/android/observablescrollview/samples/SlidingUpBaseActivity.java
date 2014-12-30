10e84f62bb23824396dd2c3d085c44b41f06467a
Soichiro Kashima <soichiro.kashima@gmail.com>
2014-12-31 00:59:54 +0900

    Improve and add more functions to SlidingUp examples. #32

--- observablescrollview-samples/src/main/java/com/github/ksoichiro/android/observablescrollview/samples/SlidingUpBaseActivity.java
+++ observablescrollview-samples/src/main/java/com/github/ksoichiro/android/observablescrollview/samples/SlidingUpBaseActivity.java
@@ -19,6 +19,7 @@ package com.github.ksoichiro.android.observablescrollview.samples;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -31,24 +32,35 @@ import com.github.ksoichiro.android.observablescrollview.Scrollable;
 import com.github.ksoichiro.android.observablescrollview.TouchInterceptionFrameLayout;
 import com.nineoldandroids.animation.ValueAnimator;
 import com.nineoldandroids.view.ViewHelper;
+import com.nineoldandroids.view.ViewPropertyAnimator;
 
 public abstract class SlidingUpBaseActivity<S extends Scrollable> extends BaseActivity implements ObservableScrollViewCallbacks {
 
     private View mHeader;
     private View mHeaderBar;
+    private View mHeaderOverlay;
+    private View mHeaderFlexibleSpace;
     private TextView mTitle;
+    private TextView mToolbarTitle;
     private View mImageView;
+    private View mFab;
+    private Toolbar mToolbar;
     private S mScrollable;
     private TouchInterceptionFrameLayout mInterceptionLayout;
     private int mActionBarSize;
     private int mIntersectionHeight;
     private int mHeaderBarHeight;
     private int mSlidingSlop;
+    private int mSlidingHeaderBlueSize;
     private int mColorPrimary;
     private float mScrollYOnDownMotion;
     private boolean mMoved;
     private float mInitialY;
     private float mMovedDistanceY;
+    private int mFabMargin;
+    private boolean mFabIsShown;
+    private int mFlexibleSpaceImageHeight;
+    private int mToolbarColor;
 
     // These flags are used for changing header colors.
     private boolean mHeaderColorIsChanging;
@@ -61,14 +73,30 @@ public abstract class SlidingUpBaseActivity<S extends Scrollable> extends BaseAc
         super.onCreate(savedInstanceState);
         setContentView(getLayoutResId());
 
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        ViewHelper.setScaleY(mToolbar, 0);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("");
+
+        mToolbarColor = getResources().getColor(R.color.primary);
+        mToolbar.setBackgroundColor(Color.TRANSPARENT);
+        mToolbar.setTitle("");
+
+        mFlexibleSpaceImageHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_image_height);
         mIntersectionHeight = getResources().getDimensionPixelSize(R.dimen.intersection_height);
         mHeaderBarHeight = getResources().getDimensionPixelSize(R.dimen.header_bar_height);
         mSlidingSlop = getResources().getDimensionPixelSize(R.dimen.sliding_slop);
         mActionBarSize = getActionBarSize();
         mColorPrimary = getResources().getColor(R.color.primary);
+        mSlidingHeaderBlueSize = getResources().getDimensionPixelSize(R.dimen.sliding_overlay_blur_size);
 
         mHeader = findViewById(R.id.header);
         mHeaderBar = findViewById(R.id.header_bar);
+        mHeaderOverlay = findViewById(R.id.header_overlay);
+        mHeaderFlexibleSpace = findViewById(R.id.header_flexible_space);
         mImageView = findViewById(R.id.image);
         mImageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,10 +106,17 @@ public abstract class SlidingUpBaseActivity<S extends Scrollable> extends BaseAc
         });
         mScrollable = createScrollable();
 
+        mFab = findViewById(R.id.fab);
+        mFabMargin = getResources().getDimensionPixelSize(R.dimen.margin_standard);
+        mFabIsShown = true;
+
         mInterceptionLayout = (TouchInterceptionFrameLayout) findViewById(R.id.scroll_wrapper);
         mInterceptionLayout.setScrollInterceptionListener(mInterceptionListener);
         mTitle = (TextView) findViewById(R.id.title);
         mTitle.setText(getTitle());
+        mToolbarTitle = (TextView) findViewById(R.id.toolbar_title);
+        mToolbarTitle.setText(mTitle.getText());
+        ViewHelper.setAlpha(mToolbarTitle, 0);
         ViewHelper.setTranslationY(mTitle, (mHeaderBarHeight - mActionBarSize) / 2);
 
         ScrollUtils.addOnGlobalLayoutListener(mInterceptionLayout, new Runnable() {
@@ -89,12 +124,18 @@ public abstract class SlidingUpBaseActivity<S extends Scrollable> extends BaseAc
             public void run() {
                 ViewHelper.setTranslationY(mInterceptionLayout, getScreenHeight() - mHeaderBarHeight);
                 ViewHelper.setTranslationY(mImageView, getScreenHeight() - mHeaderBarHeight);
+                if (mFab != null) {
+                    ViewHelper.setTranslationX(mFab, mTitle.getWidth() - mFabMargin - mFab.getWidth());
+                    ViewHelper.setTranslationY(mFab, ViewHelper.getX(mTitle) - (mFab.getHeight() / 2));
+                }
                 changeHeaderBarColorAnimated(false);
+                changeHeaderOverlay();
             }
         });
     }
 
     protected abstract int getLayoutResId();
+
     protected abstract S createScrollable();
 
     @Override
@@ -221,7 +262,32 @@ public abstract class SlidingUpBaseActivity<S extends Scrollable> extends BaseAc
         float imageTranslationY = Math.max(0, imageAnimatableHeight - (imageAnimatableHeight - translationY) * imageTranslationScale);
         ViewHelper.setTranslationY(mImageView, imageTranslationY);
 
+        // Show/hide FAB
+        if (ViewHelper.getTranslationY(mInterceptionLayout) < mFlexibleSpaceImageHeight) {
+            hideFab();
+        } else {
+            ViewPropertyAnimator.animate(mToolbar).scaleY(0).setDuration(200).start();
+            showFab();
+        }
+        if (ViewHelper.getTranslationY(mInterceptionLayout) <= mFlexibleSpaceImageHeight) {
+            ViewPropertyAnimator.animate(mToolbar).scaleY(1).setDuration(200).start();
+            mToolbar.setBackgroundColor(ScrollUtils.getColorWithAlpha(0, mToolbarColor));
+        }
+
+        if (ViewHelper.getTranslationY(mInterceptionLayout) <= mIntersectionHeight) {
+            if (ViewHelper.getAlpha(mToolbarTitle) == 0) {
+                ViewPropertyAnimator.animate(mToolbarTitle).cancel();
+                ViewPropertyAnimator.animate(mToolbarTitle).alpha(1).setDuration(200).start();
+            }
+        } else if (ViewHelper.getAlpha(mToolbarTitle) == 1) {
+            ViewPropertyAnimator.animate(mToolbarTitle).cancel();
+            ViewPropertyAnimator.animate(mToolbarTitle).alpha(0).setDuration(200).start();
+        } else {
+            ViewHelper.setAlpha(mToolbarTitle, 0);
+        }
+
         changeHeaderBarColorAnimated(true);
+        changeHeaderOverlay();
     }
 
     private void slideWithAnimation(float toY) {
@@ -284,6 +350,34 @@ public abstract class SlidingUpBaseActivity<S extends Scrollable> extends BaseAc
         mHeaderBar.setBackgroundColor(ScrollUtils.mixColors(mColorPrimary, Color.WHITE, alpha));
         mTitle.setTextColor(ScrollUtils.mixColors(Color.WHITE, Color.BLACK, alpha));
         mHeaderColorChangedToBottom = (alpha == 1);
+    }
+
+    private void changeHeaderOverlay() {
+        final float translationY = ViewHelper.getTranslationY(mInterceptionLayout);
+        if (translationY <= mToolbar.getHeight() - mSlidingHeaderBlueSize) {
+            mHeaderOverlay.setVisibility(View.VISIBLE);
+            mHeaderFlexibleSpace.getLayoutParams().height = (int) (mToolbar.getHeight() - mSlidingHeaderBlueSize - translationY);
+            mHeaderFlexibleSpace.requestLayout();
+            mHeaderOverlay.requestLayout();
+        } else {
+            mHeaderOverlay.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void showFab() {
+        if (!mFabIsShown && mFab != null) {
+            ViewPropertyAnimator.animate(mFab).cancel();
+            ViewPropertyAnimator.animate(mFab).scaleX(1).scaleY(1).setDuration(200).start();
+            mFabIsShown = true;
+        }
+    }
+
+    private void hideFab() {
+        if (mFabIsShown && mFab != null) {
+            ViewPropertyAnimator.animate(mFab).cancel();
+            ViewPropertyAnimator.animate(mFab).scaleX(0).scaleY(0).setDuration(200).start();
+            mFabIsShown = false;
+        }
     }
 
     private float getAnchorYBottom() {
